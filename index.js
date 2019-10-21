a88a1e9208ca4c421e36feb6d482bb3443186c33
no_connection <muhammadsaidarrafi@gmail.com>
2019-10-21 12:29:52 +0700

    allow origin

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.use(taskRouter)
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
     });
 

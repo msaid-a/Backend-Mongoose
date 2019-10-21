466fe6d6ce47f93e5a7c1656c1b06f335913f7ca
no_connection <muhammadsaidarrafi@gmail.com>
2019-10-21 12:18:46 +0700

    allow origin

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
+
 app.get('/', (req,res)=>{
     res.send(`<h1> API Running at ${port}</h1>`)
 })

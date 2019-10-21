2273c1bc7567d9d3ea31879e13897d8d05d7235f
no_connection <muhammadsaidarrafi@gmail.com>
2019-10-21 12:26:12 +0700

    allow origin

--- index.js
+++ index.js
@@ -23,19 +23,11 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-var whitelist = ['http://example1.com', 'http://example2.com']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
-// Then pass them to cors:
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 
 app.get('/', (req,res)=>{
     res.send(`<h1> API Running at ${port}</h1>`)
